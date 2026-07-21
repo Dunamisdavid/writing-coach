436a0de893c41e60ed3aef29ecf89a33f8ac1e6c
David <oloruntobifaleye@gmail.com>
2026-07-21 05:11:22 +0100

    Fix migration SQL file

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,8 +1,3 @@
-◇ injected env (2) from .env.local // tip: ⌁ auth for agents [www.vestauth.com]
--- CreateSchema
-CREATE SCHEMA IF NOT EXISTS "public";
-
--- CreateTable
 CREATE TABLE "Entry" (
     "id" TEXT NOT NULL,
     "prompt" TEXT NOT NULL,
@@ -14,4 +9,3 @@ CREATE TABLE "Entry" (
 
     CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
 );
-
